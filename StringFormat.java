73c6209bb03a9f8e0fc501afa1950eca37c92873
pokm321 <31551276+pokm321@users.noreply.github.com>
2022-05-20 09:56:04 +0900

    Create StringFormat.java

---
+++ StringFormat.java
@@ -0,0 +1,31 @@
+package algorithms;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class StringFormat {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		ArrayList<String> list = new ArrayList<String>();
+		
+		int number = sc.nextInt();
+		sc.nextLine();
+		
+		for (int i = 0; i < number; i++) {
+			String name = sc.nextLine();
+			list.add(name);
+		}
+		
+		System.out.print("Hello ");
+		
+		System.out.print(list.get(0));
+		for (int i = 1; i < list.size(); i++) {
+			System.out.print("," + list.get(i));			
+		}
+		
+		System.out.print(".");
+	}
+
+}
